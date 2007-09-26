0fbe847ac122e7f4fcb1241ed018f595e00baffb
guillaume.bilodeau <guillaume.bilodeau@c9a5f7a1-833a-0410-b735-d5c5875e24e8>
2007-09-26 18:18:33 +0000

    Simple food update (web controllers)

--- gb1-healthcheck/src/main/java/com/gb1/healthcheck/web/nutrition/SimpleFoodCreationRequest.java
+++ gb1-healthcheck/src/main/java/com/gb1/healthcheck/web/nutrition/SimpleFoodCreationRequest.java
@@ -16,20 +16,20 @@ public class SimpleFoodCreationRequest implements SimpleFoodPropertyProvider {
 	public SimpleFoodCreationRequest() {
 	}
 
-	public Group getGroup() {
-		return group;
-	}
-
-	public void setGroup(Group group) {
-		this.group = group;
-	}
-
 	public String getName() {
 		return name;
 	}
 
 	public void setName(String name) {
 		this.name = name;
+	}
+
+	public Group getGroup() {
+		return group;
+	}
+
+	public void setGroup(Group group) {
+		this.group = group;
 	}
 
 	public void setSelectedNutrients(String[] nutrientNames) {
