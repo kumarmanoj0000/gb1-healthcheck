c2202a605d37cdc768dbe1b38794ccfe11c2611a
guillaume.bilodeau <guillaume.bilodeau@c9a5f7a1-833a-0410-b735-d5c5875e24e8>
2007-09-30 16:50:48 +0000

    Simplified enums

--- gb1-healthcheck/src/main/java/com/gb1/healthcheck/domain/nutrition/PreparationMethod.java
+++ gb1-healthcheck/src/main/java/com/gb1/healthcheck/domain/nutrition/PreparationMethod.java
@@ -1,15 +1,5 @@
 package com.gb1.healthcheck.domain.nutrition;
 
 public enum PreparationMethod {
-	RAW("raw"), BOILED("boiled"), STEWED("stewed"), SAUTEED("sauteed"), GRILLED("grilled");
-
-	private String name;
-
-	PreparationMethod(String name) {
-		this.name = name;
-	}
-
-	public String getName() {
-		return name;
-	}
+	RAW, BOILED, STEWED, SAUTEED, GRILLED;
 }
