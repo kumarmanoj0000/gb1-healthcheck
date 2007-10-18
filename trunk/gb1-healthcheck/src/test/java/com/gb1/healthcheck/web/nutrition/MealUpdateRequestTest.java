cc1b05a4cd9346f7c1e3958e57d43c37c30ed33f
guillaume.bilodeau <guillaume.bilodeau@c9a5f7a1-833a-0410-b735-d5c5875e24e8>
2007-10-18 21:04:41 +0000

    Meal update: web

---
+++ trunk/gb1-healthcheck/src/test/java/com/gb1/healthcheck/web/nutrition/MealUpdateRequestTest.java
@@ -0,0 +1,33 @@
+package com.gb1.healthcheck.web.nutrition;
+
+import java.util.Arrays;
+import java.util.Date;
+
+import junit.framework.TestCase;
+
+import com.gb1.healthcheck.domain.nutrition.Meal;
+import com.gb1.healthcheck.domain.nutrition.Meals;
+import com.gb1.healthcheck.domain.nutrition.PreparedFood;
+
+public class MealUpdateRequestTest extends TestCase {
+	public void testNewRequestFromMeal() {
+		final Date now = new Date();
+		final Meal meal = new Meal(now).addDish(Meals.spaghettiDish())
+				.addDish(Meals.redWineDrink());
+		MealUpdateRequest req = new MealUpdateRequest(meal);
+
+		int i = 0;
+		Long[] selectedFoodIds = new Long[meal.getDishes().size()];
+		String[] selectedPrepMethodNames = new String[meal.getDishes().size()];
+
+		for (PreparedFood dish : meal.getDishes()) {
+			selectedFoodIds[i] = dish.getIngredient().getId();
+			selectedPrepMethodNames[i] = dish.getPreparationMethod().name();
+			i++;
+		}
+
+		assertEquals(meal.getInstant(), req.getInstant());
+		assertTrue(Arrays.equals(selectedFoodIds, req.getSelectedFoodIds()));
+		assertTrue(Arrays.equals(selectedPrepMethodNames, req.getSelectedPreparationMethodNames()));
+	}
+}
