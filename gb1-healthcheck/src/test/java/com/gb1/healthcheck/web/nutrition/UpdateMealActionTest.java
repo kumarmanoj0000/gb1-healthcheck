c5b2df9cc595a9b9963b29d24aadcd8e3064a603
guillaume.bilodeau <guillaume.bilodeau@c9a5f7a1-833a-0410-b735-d5c5875e24e8>
2007-10-19 13:53:58 +0000

    Meal update: web

--- gb1-healthcheck/src/test/java/com/gb1/healthcheck/web/nutrition/UpdateMealActionTest.java
+++ gb1-healthcheck/src/test/java/com/gb1/healthcheck/web/nutrition/UpdateMealActionTest.java
@@ -10,6 +10,7 @@ import org.easymock.EasyMock;
 
 import com.gb1.healthcheck.domain.nutrition.FullMealHydrater;
 import com.gb1.healthcheck.domain.nutrition.Meal;
+import com.gb1.healthcheck.domain.nutrition.MealException;
 import com.gb1.healthcheck.domain.nutrition.Meals;
 import com.gb1.healthcheck.services.nutrition.MealService;
 import com.opensymphony.xwork2.Action;
@@ -63,6 +64,31 @@ public class UpdateMealActionTest extends TestCase {
 		assertEquals(Action.SUCCESS, result);
 		assertFalse(session.containsKey(MODEL_SESSION_KEY));
 		EasyMock.verify(mealSvc);
+	}
+
+	@SuppressWarnings("unchecked")
+	public void testUpdateWithErrors() throws MealException {
+		Meal meal = Meals.fullItalianDinner();
+		MealUpdateRequest model = new MealUpdateRequest(meal);
+
+		Map session = new HashMap();
+		session.put(MODEL_SESSION_KEY, model);
+
+		MealService mealSvc = EasyMock.createMock(MealService.class);
+		mealSvc.updateMeal(meal.getId(), model);
+		EasyMock.expectLastCall().andThrow(new MealException() {});
+		EasyMock.replay(mealSvc);
+
+		UpdateMealAction action = new UpdateMealAction();
+		action.setMealService(mealSvc);
+
+		action.setSession(session);
+		action.setMealId(meal.getId());
+		String result = action.update();
+
+		assertEquals(Action.INPUT, result);
+		assertTrue(action.hasActionErrors());
+		assertTrue(session.containsKey(MODEL_SESSION_KEY));
 	}
 
 	public void testCancel() {
