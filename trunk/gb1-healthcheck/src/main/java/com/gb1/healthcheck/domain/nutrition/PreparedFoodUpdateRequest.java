f0bf0e1f73eda0467aa00b92a309a8a7c2f3e43c
guillaume.bilodeau <guillaume.bilodeau@c9a5f7a1-833a-0410-b735-d5c5875e24e8>
2007-10-28 16:36:40 +0000

    Property provider evolution: meals

---
+++ trunk/gb1-healthcheck/src/main/java/com/gb1/healthcheck/domain/nutrition/PreparedFoodUpdateRequest.java
@@ -0,0 +1,7 @@
+package com.gb1.healthcheck.domain.nutrition;
+
+public interface PreparedFoodUpdateRequest {
+	Long getIngredientId();
+
+	PreparationMethod getPreparationMethod();
+}
--- trunk/gb1-healthcheck/src/main/java/com/gb1/healthcheck/web/nutrition/BasicMealCreationRequest.java
+++ trunk/gb1-healthcheck/src/main/java/com/gb1/healthcheck/web/nutrition/BasicMealCreationRequest.java
@@ -1,17 +1,6 @@
 package com.gb1.healthcheck.web.nutrition;
 
-import java.util.HashSet;
-import java.util.Set;
-
-import com.gb1.healthcheck.domain.nutrition.MealCreationRequest;
-import com.gb1.healthcheck.domain.nutrition.PreparedFoodCreationRequest;
-
-public class BasicMealCreationRequest extends MealRequestSupport implements MealCreationRequest {
+public class BasicMealCreationRequest extends MealRequestSupport {
 	public BasicMealCreationRequest() {
-	}
-
-	public Set<PreparedFoodCreationRequest> getPreparedFoodCreationRequests() {
-		Set<PreparedFoodCreationRequest> requests = new HashSet<PreparedFoodCreationRequest>();
-		return requests;
 	}
 }
