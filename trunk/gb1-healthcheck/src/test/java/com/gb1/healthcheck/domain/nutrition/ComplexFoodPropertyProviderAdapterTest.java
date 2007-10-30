379e59740368ce277eed14324b839e9e2e84260d
guillaume.bilodeau <guillaume.bilodeau@c9a5f7a1-833a-0410-b735-d5c5875e24e8>
2007-10-30 01:00:13 +0000

    Property provider evolution: tests

---
+++ trunk/gb1-healthcheck/src/test/java/com/gb1/healthcheck/domain/nutrition/ComplexFoodPropertyProviderAdapterTest.java
@@ -0,0 +1,41 @@
+package com.gb1.healthcheck.domain.nutrition;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+import junit.framework.TestCase;
+
+import org.apache.commons.collections.CollectionUtils;
+import org.easymock.EasyMock;
+
+public class ComplexFoodPropertyProviderAdapterTest extends TestCase {
+	public void testAdapt() {
+		final String name = "name";
+		final Map<Long, Food> foods = new HashMap<Long, Food>();
+		foods.put(Foods.beef().getId(), Foods.beef());
+		foods.put(Foods.apple().getId(), Foods.apple());
+
+		ComplexFoodCreationRequest request = new ComplexFoodCreationRequest() {
+			public Set<Long> getIngredientIds() {
+				return foods.keySet();
+			}
+
+			public String getName() {
+				return name;
+			}
+		};
+
+		FoodRepository foodRepo = EasyMock.createMock(FoodRepository.class);
+		for (Long id : foods.keySet()) {
+			EasyMock.expect(foodRepo.loadFood(id)).andReturn(foods.get(id));
+		}
+		EasyMock.replay(foodRepo);
+
+		ComplexFoodPropertyProviderAdapter adapter = new ComplexFoodPropertyProviderAdapter(request);
+		adapter.setFoodRepository(foodRepo);
+
+		assertEquals(name, adapter.getName());
+		assertTrue(CollectionUtils.isEqualCollection(foods.values(), adapter.getIngredients()));
+	}
+}
