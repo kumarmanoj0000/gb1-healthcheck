be68d04daf999ee03bedb545735d8350c52a7672
guillaume.bilodeau <guillaume.bilodeau@c9a5f7a1-833a-0410-b735-d5c5875e24e8>
2007-11-13 03:36:59 +0000

    Refactoring

--- gb1-healthcheck/src/main/java/com/gb1/healthcheck/web/utils/DateTimeConverter.java
+++
@@ -1,9 +0,0 @@
-package com.gb1.healthcheck.web.utils;
-
-public class DateTimeConverter extends DateConverter {
-	public static final String DEFAULT_DATE_TIME_FORMAT = "yyyy-MM-dd hh:mm:ss";
-
-	public DateTimeConverter() {
-		super(DEFAULT_DATE_TIME_FORMAT);
-	}
-}
