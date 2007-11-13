c2f2eb65071ac5f69f1a5b8b95c914e150d5c2bb
guillaume.bilodeau <guillaume.bilodeau@c9a5f7a1-833a-0410-b735-d5c5875e24e8>
2007-11-13 03:36:59 +0000

    Refactoring

--- trunk/gb1-healthcheck/src/test/java/com/gb1/healthcheck/web/utils/DateConverterTest.java
+++ trunk/gb1-healthcheck/src/test/java/com/gb1/healthcheck/web/utils/DateConverterTest.java
@@ -7,19 +7,21 @@ import junit.framework.TestCase;
 
 import org.apache.commons.lang.time.DateUtils;
 
+import com.gb1.healthcheck.web.meals.MealInstantConverter;
+
 public class DateConverterTest extends TestCase {
 	public void testConvertFromString() throws ParseException {
 		String text = "2007-10-18 11:00:00";
 		Date expectedInstant = parseInstant(text);
 
-		DateConverter c = new DateConverter(DateTimeConverter.DEFAULT_DATE_TIME_FORMAT);
+		DateConverter c = new DateConverter(MealInstantConverter.DEFAULT_DATE_TIME_FORMAT);
 		Date date = c.convertFromString(null, new String[] { text }, null);
 
 		assertEquals(expectedInstant, date);
 	}
 
 	public void testConvertFromStringInvalidInputs() {
-		DateConverter c = new DateConverter(DateTimeConverter.DEFAULT_DATE_TIME_FORMAT);
+		DateConverter c = new DateConverter(MealInstantConverter.DEFAULT_DATE_TIME_FORMAT);
 
 		assertNull(c.convertFromString(null, null, null));
 		assertNull(c.convertFromString(null, new String[] {}, null));
@@ -30,14 +32,14 @@ public class DateConverterTest extends TestCase {
 		String expectedText = "2007-10-18 11:00:00";
 		Date date = parseInstant(expectedText);
 
-		DateConverter c = new DateConverter(DateTimeConverter.DEFAULT_DATE_TIME_FORMAT);
+		DateConverter c = new DateConverter(MealInstantConverter.DEFAULT_DATE_TIME_FORMAT);
 		String text = c.convertToString(null, date);
 
 		assertEquals(expectedText, text);
 	}
 
 	public void testConvertToStringInvalidInputs() {
-		DateConverter c = new DateConverter(DateTimeConverter.DEFAULT_DATE_TIME_FORMAT);
+		DateConverter c = new DateConverter(MealInstantConverter.DEFAULT_DATE_TIME_FORMAT);
 
 		assertEquals("", c.convertToString(null, null));
 		assertEquals("", c.convertToString(null, "Not a date"));
@@ -45,6 +47,6 @@ public class DateConverterTest extends TestCase {
 
 	private Date parseInstant(String text) throws ParseException {
 		return DateUtils.parseDate(text,
-				new String[] { DateTimeConverter.DEFAULT_DATE_TIME_FORMAT });
+				new String[] { MealInstantConverter.DEFAULT_DATE_TIME_FORMAT });
 	}
 }
