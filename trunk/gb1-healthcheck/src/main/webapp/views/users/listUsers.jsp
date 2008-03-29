6bd3575b4686ccd4102ffbfe927ccce185469293
guillaume.bilodeau <guillaume.bilodeau@c9a5f7a1-833a-0410-b735-d5c5875e24e8>
2008-03-29 15:48:57 +0000

    User list: paging, complete sorting and cached list

--- trunk/gb1-healthcheck/src/main/webapp/views/users/listUsers.jsp
+++ trunk/gb1-healthcheck/src/main/webapp/views/users/listUsers.jsp
@@ -4,9 +4,12 @@
 <%@ include file="/views/fragments/actionMessages.jsp"%>
 <h2><s:text name="users.list.title" /></h2>
 
+<s:set name="userListPageSize" value="@com.gb1.healthcheck.web.WebConstants@DEFAULT_PAGE_SIZE" />
 <s:url id="listUsersUrl" namespace="/users" action="listUsers" />
 
-<display:table name="users" pagesize="10" requestURI="${listUsersUrl}" class="listTable" defaultsort="1">
+<display:table
+		name="users" pagesize="${userListPageSize}" requestURI="${listUsersUrl}" class="listTable"
+		sort="list" defaultsort="1">
 	<display:column property="login" sortable="true" />
 	<display:column property="email" sortable="true" />
 </display:table>
--- trunk/gb1-healthcheck/src/test/java/com/gb1/healthcheck/web/users/ListUsersActionTest.java
+++ trunk/gb1-healthcheck/src/test/java/com/gb1/healthcheck/web/users/ListUsersActionTest.java
@@ -1,12 +1,42 @@
 package com.gb1.healthcheck.web.users;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import junit.framework.TestCase;
 
+import org.easymock.EasyMock;
+
+import com.gb1.healthcheck.domain.users.User;
+import com.gb1.healthcheck.domain.users.Users;
+import com.gb1.healthcheck.services.users.UserService;
 import com.opensymphony.xwork2.Action;
 
 public class ListUsersActionTest extends TestCase {
 	public void testExecute() {
+		List<User> allUsers = new ArrayList<User>(Users.all());
+
+		UserService userSvc = EasyMock.createMock(UserService.class);
+		EasyMock.expect(userSvc.getAllUsers()).andReturn(Users.all()).once();
+		EasyMock.replay(userSvc);
+
+		Map<String, Object> sessionMap = new HashMap<String, Object>();
+
 		ListUsersAction action = new ListUsersAction();
+		action.setSession(sessionMap);
+		action.setUserService(userSvc);
+
 		assertEquals(Action.SUCCESS, action.execute());
+		assertEquals(allUsers, action.getUsers());
+		assertTrue(sessionMap.containsValue(allUsers));
+
+		assertEquals(Action.SUCCESS, action.execute());
+		assertEquals(allUsers, action.getUsers());
+		assertTrue(sessionMap.containsValue(allUsers));
+
+		// make sure that loading the user list occurs only once
+		EasyMock.verify(userSvc);
 	}
 }
