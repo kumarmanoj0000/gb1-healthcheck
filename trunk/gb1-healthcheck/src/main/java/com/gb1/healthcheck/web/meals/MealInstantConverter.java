d4e06d835830fcb9dc593fa418da93a5b935741f
guillaume.bilodeau <guillaume.bilodeau@c9a5f7a1-833a-0410-b735-d5c5875e24e8>
2008-04-04 03:27:57 +0000

    Meals: fixed date issues

--- trunk/gb1-healthcheck/src/main/java/com/gb1/healthcheck/web/meals/MealInstantConverter.java
+++ trunk/gb1-healthcheck/src/main/java/com/gb1/healthcheck/web/meals/MealInstantConverter.java
@@ -3,9 +3,7 @@ package com.gb1.healthcheck.web.meals;
 import com.gb1.healthcheck.web.utils.DateConverter;
 
 public class MealInstantConverter extends DateConverter {
-	public static final String DEFAULT_DATE_TIME_FORMAT = "yyyy-MM-dd hh:mm:ss";
-
 	public MealInstantConverter() {
-		super(DEFAULT_DATE_TIME_FORMAT);
+		super("yyyy-MM-dd HH:mm");
 	}
 }
--- trunk/gb1-healthcheck/src/test/java/com/gb1/healthcheck/web/utils/DateConverterTest.java
+++ trunk/gb1-healthcheck/src/test/java/com/gb1/healthcheck/web/utils/DateConverterTest.java
@@ -11,17 +11,17 @@ import com.gb1.healthcheck.web.meals.MealInstantConverter;
 
 public class DateConverterTest extends TestCase {
 	public void testConvertFromString() throws ParseException {
-		String text = "2007-10-18 11:00:00";
+		String text = "2007-10-18 11:00";
 		Date expectedInstant = parseInstant(text);
 
-		DateConverter c = new DateConverter(MealInstantConverter.DEFAULT_DATE_TIME_FORMAT);
+		DateConverter c = new MealInstantConverter();
 		Date date = c.convertFromString(null, new String[] { text }, null);
 
 		assertEquals(expectedInstant, date);
 	}
 
 	public void testConvertFromStringInvalidInputs() {
-		DateConverter c = new DateConverter(MealInstantConverter.DEFAULT_DATE_TIME_FORMAT);
+		DateConverter c = new MealInstantConverter();
 
 		assertNull(c.convertFromString(null, null, null));
 		assertNull(c.convertFromString(null, new String[] {}, null));
@@ -29,24 +29,23 @@ public class DateConverterTest extends TestCase {
 	}
 
 	public void testConvertToString() throws ParseException {
-		String expectedText = "2007-10-18 11:00:00";
+		String expectedText = "2007-10-18 11:00";
 		Date date = parseInstant(expectedText);
 
-		DateConverter c = new DateConverter(MealInstantConverter.DEFAULT_DATE_TIME_FORMAT);
+		DateConverter c = new MealInstantConverter();
 		String text = c.convertToString(null, date);
 
 		assertEquals(expectedText, text);
 	}
 
 	public void testConvertToStringInvalidInputs() {
-		DateConverter c = new DateConverter(MealInstantConverter.DEFAULT_DATE_TIME_FORMAT);
+		DateConverter c = new MealInstantConverter();
 
 		assertEquals("", c.convertToString(null, null));
 		assertEquals("", c.convertToString(null, "Not a date"));
 	}
 
 	private Date parseInstant(String text) throws ParseException {
-		return DateUtils.parseDate(text,
-				new String[] { MealInstantConverter.DEFAULT_DATE_TIME_FORMAT });
+		return DateUtils.parseDate(text, new String[] { "yyyy-MM-dd HH:mm" });
 	}
 }
