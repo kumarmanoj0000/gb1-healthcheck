3c5354a4f27389123e266dab7b02c8bc94024cf8
guillaume.bilodeau <guillaume.bilodeau@c9a5f7a1-833a-0410-b735-d5c5875e24e8>
2008-04-11 23:38:28 +0000

    Fixed serialization issue

--- gb1-healthcheck/src/main/java/com/gb1/healthcheck/domain/users/PasswordGeneratorImpl.java
+++ gb1-healthcheck/src/main/java/com/gb1/healthcheck/domain/users/PasswordGeneratorImpl.java
@@ -1,10 +1,12 @@
 package com.gb1.healthcheck.domain.users;
 
+import java.io.Serializable;
+
 import org.apache.commons.lang.RandomStringUtils;
 import org.springframework.stereotype.Component;
 
 @Component("passwordGenerator")
-public class PasswordGeneratorImpl implements PasswordGenerator {
+public class PasswordGeneratorImpl implements PasswordGenerator, Serializable {
 	public PasswordGeneratorImpl() {
 	}
 
